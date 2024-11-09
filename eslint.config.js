2e849fa78c3b762f404e475addbd129f6abb5ea5
João G. Bortoletto <jgb3532@hotmail.com>
2024-11-08 22:30:55 -0300

    chore: more typescript configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,28 @@
 import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts}'] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+        languageOptions: {
+            globals: globals.browser,
+            parser: tsParser,
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+            prettier: prettier,
+        },
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            ...tseslint.configs.recommended.rules,
+            ...prettierConfig.rules,
+            'prettier/prettier': 'error',
+        },
+    },
 ];
