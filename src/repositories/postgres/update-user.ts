2dc552b8272d0203d59878b17b35b39ce9578a4f
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-20 12:22:12 -0300

    feat: create PostgresUpdateUserRepository

---
+++ src/repositories/postgres/update-user.ts
@@ -0,0 +1,33 @@
+import { PostgresHelper } from '../../db/postgres/helper';
+import { FormCreateUserParams } from '../../types/user/form-create-user';
+import { User } from '../../types/user/return-user';
+
+export class PostgresUpdateUserRepository {
+    async execute(
+        userId: string,
+        updateUserParams: Partial<FormCreateUserParams>,
+    ): Promise<User> {
+        const updateFields: string[] = [];
+        const updateValues: any[] = [];
+
+        Object.keys(updateUserParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`);
+            updateValues.push(
+                updateUserParams[key as keyof FormCreateUserParams],
+            );
+        });
+
+        updateValues.push(userId);
+
+        const query = `
+            UPDATE users 
+            SET ${updateFields.join(', ')} 
+            WHERE id = $${updateValues.length} 
+            RETURNING id, first_name, last_name, email
+        `;
+
+        const updatedUser = await PostgresHelper.query(query, updateValues);
+
+        return updatedUser[0];
+    }
+}
