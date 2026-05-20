b0ed6c78991902302b297138e72e5e08a8279126
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-20 11:20:20 -0300

    feat: create GetUserByIdController

---
+++ src/controller/get-user-by-id.ts
@@ -0,0 +1,29 @@
+import { Request } from 'express';
+import validator from 'validator';
+import { DTOUser } from '../types/user/dto-user';
+import { GetUserByIdUseCase } from '../use-cases/get-user-by-id';
+import { badRequest, internalServerError, ok } from './helpers';
+
+export class GetUserByIdController {
+    async execute(request: Request): Promise<DTOUser> {
+        try {
+            const userId = request.params.userId as string;
+
+            if (!userId) return badRequest('User ID is required');
+
+            const isValidUUID = validator.isUUID(userId);
+            if (!isValidUUID) return badRequest('Invalid user ID format');
+
+            const getUserByIdUseCase = new GetUserByIdUseCase();
+
+            const user = await getUserByIdUseCase.execute(userId);
+
+            if (!user) return badRequest('User not found');
+
+            return ok(user);
+        } catch (error) {
+            console.error('Error getting user by id:', error);
+            return internalServerError();
+        }
+    }
+}
