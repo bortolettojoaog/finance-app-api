defeb34fcf91dd41de1b5dcb107f6c9b01fff59f
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-20 12:31:39 -0300

    chore: add type into get user by mail execute method

--- src/use-cases/get-user-by-mail.ts
+++ src/use-cases/get-user-by-mail.ts
@@ -1,7 +1,8 @@
 import { PostgresGetUserByMailRepository } from '../repositories/postgres/get-user-by-mail';
+import { User } from '../types/user/return-user';
 
 export class GetUserByMailUseCase {
-    async execute(email: string) {
+    async execute(email: string): Promise<User> {
         const getUserByMailRepository = new PostgresGetUserByMailRepository();
 
         const user = await getUserByMailRepository.execute(email);
