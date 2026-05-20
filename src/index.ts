93fa33d5731bd4a89905375f85577b3868d1770b
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-20 08:39:55 -0300

    feat: initialize express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import 'dotenv/config.js';
+// @ts-expect-error Express type declarations are unavailable in this environment.
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
