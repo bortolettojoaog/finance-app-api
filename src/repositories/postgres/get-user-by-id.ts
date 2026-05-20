4ccee815439525e172e8b399f67c5cfa3b5d90e9
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-20 11:20:02 -0300

    fix: solve PostegresGetUserById query to return only dto params

--- src/repositories/postgres/get-user-by-id.ts
+++ src/repositories/postgres/get-user-by-id.ts
@@ -4,7 +4,7 @@ import { User } from '../../types/user/return-user';
 export class PostgresGetUserByIdRepository {
     async execute(userId: string): Promise<User> {
         const user = await PostgresHelper.query(
-            'SELECT * FROM users WHERE id = $1',
+            'SELECT id, first_name, last_name, email FROM users WHERE id = $1',
             [userId],
         );
 
