99ae9e37d7b4004691a7dce260d958ba56af895d
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-20 11:20:37 -0300

    feat: create get user by id request

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config.js';
 import express, { Request, Response } from 'express';
 import { CreateUserController } from './controller/create-user';
+import { GetUserByIdController } from './controller/get-user-by-id';
 
 const app = express();
 
@@ -11,6 +12,15 @@ app.post('/api/users', async (request: Request, response: Response) => {
 
     const { status_code, body, error } =
         await createUserController.execute(request);
+
+    return response.status(status_code).json(error ? { error } : body);
+});
+
+app.get('/api/users/:userId', async (request: Request, response: Response) => {
+    const getUserByIdController = new GetUserByIdController();
+
+    const { status_code, body, error } =
+        await getUserByIdController.execute(request);
 
     return response.status(status_code).json(error ? { error } : body);
 });
