289468da656ce8cc37b410af9104800a63a55f61
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-20 10:55:49 -0300

    feat: create controller response helpers

---
+++ src/controller/helpers.ts
@@ -0,0 +1,23 @@
+export const badRequest = (message: string) => {
+    return {
+        status_code: 400,
+        error: message,
+        body: null,
+    };
+};
+
+export const created = (body: any) => {
+    return {
+        status_code: 201,
+        error: null,
+        body,
+    };
+};
+
+export const internalServerError = (message?: string) => {
+    return {
+        status_code: 500,
+        error: message || 'Internal server error',
+        body: null,
+    };
+};
