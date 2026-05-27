1e20a98bd4b74bc87d6257dd5cafb408e9329809
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-27 17:45:32 -0300

    feat: create DeleteTransactionController

---
+++ src/controller/transaction/delete-transation.ts
@@ -0,0 +1,45 @@
+import { Request } from 'express';
+import { TransactionNotFoundError } from '../../errors';
+import { DTOTransaction, Transaction } from '../../types';
+import {
+    checkIfTransactionIdIsValid,
+    internalServerError,
+    invalidTransactionIdResponse,
+    notFound,
+    ok,
+} from '../helpers';
+
+interface IDeleteTransactionUseCase {
+    execute(TransactionId: string): Promise<Transaction>;
+}
+
+export class DeleteTransactionController {
+    private readonly deleteTransactionUseCase: IDeleteTransactionUseCase;
+
+    constructor(deleteTransactionUseCase: IDeleteTransactionUseCase) {
+        this.deleteTransactionUseCase = deleteTransactionUseCase;
+    }
+
+    async execute(httpRequest: Request): Promise<DTOTransaction> {
+        try {
+            const transactionId = httpRequest.params.transactionId as string;
+
+            const isValidUUID = checkIfTransactionIdIsValid(transactionId);
+
+            if (!isValidUUID) return invalidTransactionIdResponse();
+
+            const deletedTransaction =
+                await this.deleteTransactionUseCase.execute(transactionId);
+
+            return ok(deletedTransaction);
+        } catch (error) {
+            console.log('Error deleting transaction:', error);
+
+            if (error instanceof TransactionNotFoundError) {
+                return notFound(error.message);
+            }
+
+            return internalServerError();
+        }
+    }
+}
--- src/controller/transaction/index.ts
+++ src/controller/transaction/index.ts
@@ -1,3 +1,5 @@
+export * from './check-deleted-transaction';
 export * from './create-transaction';
+export * from './delete-transation';
 export * from './get-transaction-by-id';
 export * from './update-transaction';
