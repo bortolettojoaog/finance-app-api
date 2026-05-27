eb05b92bb39676fb13735442a51fa838e5fe17d0
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-27 17:34:00 -0300

    feat: create CheckDeletedTransactionController

---
+++ src/controller/transaction/check-deleted-transaction.ts
@@ -0,0 +1,44 @@
+import { DeletedTransaction, DTODeletedTransaction } from '../../types';
+import {
+    checkIfTransactionIdIsValid,
+    internalServerError,
+    invalidTransactionIdResponse,
+    ok,
+} from '../helpers';
+
+import { Request } from 'express';
+
+interface ICheckDeletedTransactionUseCase {
+    execute(transactionId: string): Promise<DeletedTransaction>;
+}
+
+export class CheckDeletedTransactionController {
+    private readonly checkDeletedTransactionUseCase: ICheckDeletedTransactionUseCase;
+
+    constructor(
+        checkDeletedTransactionUseCase: ICheckDeletedTransactionUseCase,
+    ) {
+        this.checkDeletedTransactionUseCase = checkDeletedTransactionUseCase;
+    }
+
+    async execute(httpRequest: Request): Promise<DTODeletedTransaction> {
+        try {
+            const transactionId = httpRequest.params.transactionId as string;
+
+            const isValidUUID = checkIfTransactionIdIsValid(transactionId);
+
+            if (!isValidUUID) return invalidTransactionIdResponse();
+
+            const isDeleted =
+                await this.checkDeletedTransactionUseCase.execute(
+                    transactionId,
+                );
+
+            return ok(isDeleted);
+        } catch (error) {
+            console.error('Error creating Transaction:', error);
+
+            return internalServerError();
+        }
+    }
+}
