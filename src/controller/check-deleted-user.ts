c681fa3846ae178e8d8c3e3ef3a21220aa9a6efa
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-26 21:58:39 -0300

    feat: create CheckDeleteUserController

---
+++ src/controller/check-deleted-user.ts
@@ -0,0 +1,40 @@
+import { DeletedUser, DTODeletedUser } from '../types/user';
+import {
+    checkIfUserIdIsValid,
+    internalServerError,
+    invalidUserIdResponse,
+    ok,
+} from './helpers';
+
+import { Request } from 'express';
+
+interface ICheckDeletedUserController {
+    execute(userId: string): Promise<DeletedUser>;
+}
+
+export class CheckDeletedUserController {
+    private readonly checkDeletedUserUseCase: ICheckDeletedUserController;
+
+    constructor(checkDeletedUserUseCase: ICheckDeletedUserController) {
+        this.checkDeletedUserUseCase = checkDeletedUserUseCase;
+    }
+
+    async execute(request: Request): Promise<DTODeletedUser> {
+        try {
+            const userId = request.params.userId as string;
+
+            const isValidUUID = checkIfUserIdIsValid(userId);
+
+            if (!isValidUUID) return invalidUserIdResponse();
+
+            const isDeleted =
+                await this.checkDeletedUserUseCase.execute(userId);
+
+            return ok(isDeleted);
+        } catch (error) {
+            console.error('Error creating user:', error);
+
+            return internalServerError();
+        }
+    }
+}
