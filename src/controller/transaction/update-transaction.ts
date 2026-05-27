5ff8f75281a0a63d2ec74cb5ae17d73ef1d664db
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-27 16:55:43 -0300

    feat: create UpdateTransactionController

---
+++ src/controller/transaction/update-transaction.ts
@@ -0,0 +1,100 @@
+import { Request } from 'express';
+import { TransactionNotFoundError } from '../../errors';
+import {
+    DTOTransaction,
+    FormUpdateTransactionParams,
+    Transaction,
+} from '../../types';
+import {
+    allFieldAreEmptyResponse,
+    checkIfTransactionIdIsValid,
+    checkTransactionAmount,
+    checkTransactionAmountIsCurrency,
+    checkTransactionType,
+    internalServerError,
+    invalidTransactionAmountResponse,
+    invalidTransactionIdResponse,
+    invalidTransactionTypeResponse,
+    negativeOrNaNTransactionAmountResponse,
+    notFoundTransationResponse,
+    ok,
+    someFieldNotAllowedResponse,
+} from '../helpers';
+
+interface IUpdateTransactionUseCase {
+    execute(
+        TransactionId: string,
+        updateTransactionParams: Partial<FormUpdateTransactionParams>,
+    ): Promise<Transaction>;
+}
+
+export class UpdateTransactionController {
+    private readonly updateTransactionUseCase: IUpdateTransactionUseCase;
+
+    constructor(updateTransactionUseCase: IUpdateTransactionUseCase) {
+        this.updateTransactionUseCase = updateTransactionUseCase;
+    }
+
+    async execute(httpRequest: Request): Promise<DTOTransaction> {
+        try {
+            const transactionId = httpRequest.params.transactionId as string;
+
+            const isValidUUID = checkIfTransactionIdIsValid(transactionId);
+
+            if (!isValidUUID) return invalidTransactionIdResponse();
+
+            const allFieldsAreEmpty =
+                Object.keys(httpRequest.body).length === 0;
+
+            if (allFieldsAreEmpty) return allFieldAreEmptyResponse();
+
+            const params = httpRequest.body as FormUpdateTransactionParams;
+
+            const allowedFields = ['name', 'amount', 'date', 'type'];
+
+            const isSomeFieldNotAllowed = Object.keys(params).some(
+                (field) => !allowedFields.includes(field),
+            );
+
+            if (isSomeFieldNotAllowed) return someFieldNotAllowedResponse();
+
+            if (params.type) {
+                const isTransactionTypeValid = checkTransactionType(
+                    params.type,
+                );
+
+                if (!isTransactionTypeValid)
+                    return invalidTransactionTypeResponse();
+            }
+
+            if (params.amount) {
+                const isAmountValid = checkTransactionAmount(params.amount);
+
+                if (!isAmountValid)
+                    return negativeOrNaNTransactionAmountResponse();
+
+                const isAmountCurrency = checkTransactionAmountIsCurrency(
+                    params.amount,
+                );
+
+                if (!isAmountCurrency)
+                    return invalidTransactionAmountResponse();
+            }
+
+            const updatedTransaction =
+                await this.updateTransactionUseCase.execute(
+                    transactionId,
+                    params,
+                );
+
+            return ok(updatedTransaction);
+        } catch (error) {
+            console.error('Error updating Transaction:', error);
+
+            if (error instanceof TransactionNotFoundError)
+                return notFoundTransationResponse();
+
+            return internalServerError();
+        }
+    }
+}
