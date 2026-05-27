a27d7bf63242342b1f8e02b16c34648330262ed8
João G. Bortoletto <jgb3532@hotmail.com>
2026-05-27 09:22:36 -0300

    fix: add missing information to PostgresCreateTransactionRepository

--- src/repositories/postgres/transaction/create-transaction.ts
+++ src/repositories/postgres/transaction/create-transaction.ts
@@ -9,13 +9,14 @@ export class PostgresCreateTransactionRepository {
         createTransactionParams: CreateTransactionParams,
     ): Promise<Transaction> {
         const transaction = await PostgresHelper.query(
-            'INSERT INTO transactions (id, user_id, amount, description, type) VALUES ($1, $2, $3, $4, $5) RETURNING id, user_id, name, date, amount, type',
+            'INSERT INTO transactions (id, user_id, amount, date, type, name) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, user_id, name, date, amount, type',
             [
                 createTransactionParams.id,
                 createTransactionParams.user_id,
                 createTransactionParams.amount,
-                createTransactionParams.name,
+                createTransactionParams.date,
                 createTransactionParams.type,
+                createTransactionParams.name,
             ],
         );
 
